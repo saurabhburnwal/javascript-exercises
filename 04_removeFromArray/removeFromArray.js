a07a1d79bdee80ec4e0e4068a7424836185d4336
Saurabh <saurabhburnwal00@gmail.com>
2023-10-14 02:55:21 +0530

    complete the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    const removed = Array.from(arguments);
+    for (rem of removed) { 
+        const index = arr.indexOf(rem);
+        if (index > -1)
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
