bfdb573ca419ab83958bf5f5af6475686782b62b
Saurabh <saurabhburnwal00@gmail.com>
2023-10-14 03:25:45 +0530

    complete the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0 ||
+        !Number.isInteger(min) ||
+        !Number.isInteger(max))
+            return 'ERROR';
+    if (min > max)  [min, max] = [max, min];
+    const startSum = (min * (min - 1)) / 2;
+    const endSum = (max * (max + 1)) / 2;
+    return endSum - startSum;
 };
 
 // Do not edit below this line
