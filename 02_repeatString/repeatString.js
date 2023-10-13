ea7ed066b1502a1a06c42f64f1bc088c4e5c9709
Saurabh <saurabhburnwal00@gmail.com>
2023-10-14 01:58:12 +0530

    complete the exercise with the most optimal option

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, times) { 
+    if (times < 0)  return 'ERROR'; 
+    let result = '';
+    while (times != 0) { 
+        result += phrase;
+        times--;
+    }
+    return result;
 };
 
 // Do not edit below this line
